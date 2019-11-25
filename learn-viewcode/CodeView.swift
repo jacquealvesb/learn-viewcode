4055a05c5caaf80af73583c3f260f3e9446e057c
Jacqueline Alves <jacque.alves.b@gmail.com>
2019-11-25 20:52:16 -0300

    Refactoring with CodeView protocol

---
+++ learn-viewcode/CodeView.swift
@@ -0,0 +1,22 @@
+//
+//  CodeView.swift
+//  learn-viewcode
+//
+//  Created by Jacqueline Alves on 25/11/19.
+//  Copyright © 2019 jacquelinealves. All rights reserved.
+//
+
+protocol CodeView {
+    func buildViewHierarchy()
+    func setupConstraints()
+    func setupAdditionalConfiguration()
+    func setupView()
+}
+
+extension CodeView {
+    func setupView() {
+        buildViewHierarchy()
+        setupConstraints()
+        setupAdditionalConfiguration()
+    }
+}
--- learn-viewcode/ViewControllerScreen.swift
+++ learn-viewcode/ViewControllerScreen.swift
@@ -9,31 +9,42 @@
 import UIKit
 
 final class ViewControllerScreen: UIView {
+    
+    lazy var button: UIButton = {
+        let view = UIButton(frame: CGRect.zero)
+        
+        view.backgroundColor = .red
+        view.setTitle("Fetch", for: .normal)
+        view.translatesAutoresizingMaskIntoConstraints = false
+        
+        return view
+    }()
+    
     override init(frame: CGRect = .zero) {
         super.init(frame: frame)
         
-        let button = UIButton(frame: CGRect.zero)
-        button.backgroundColor = .red
-        button.setTitle("Fetch", for: .normal)
-        button.translatesAutoresizingMaskIntoConstraints = false
-        
-        // Add view
-        addSubview(button)
-        
-        // Configure constraints
-        button.leadingAnchor.constraint(equalTo: self.leadingAnchor, constant: 15).isActive = true
-        button.trailingAnchor.constraint(equalTo: self.trailingAnchor, constant: -15).isActive = true
-        button.heightAnchor.constraint(equalToConstant: 50).isActive = true
-        button.bottomAnchor.constraint(equalTo: self.bottomAnchor, constant: -20).isActive = true
-        
-        // Aditional setup
-        
-        
-        
-        backgroundColor = .darkGray
+        setupView()
     }
     
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
+    }
+}
+
+extension ViewControllerScreen: CodeView {
+    
+    func buildViewHierarchy() {
+        addSubview(button)
+    }
+
+    func setupConstraints() {
+        button.leadingAnchor.constraint(equalTo: self.leadingAnchor, constant: 15).isActive = true
+        button.trailingAnchor.constraint(equalTo: self.trailingAnchor, constant: -15).isActive = true
+        button.heightAnchor.constraint(equalToConstant: 50).isActive = true
+        button.bottomAnchor.constraint(equalTo: self.bottomAnchor, constant: -20).isActive = true
+    }
+    
+    func setupAdditionalConfiguration() {
+        backgroundColor = .darkGray
     }
 }
